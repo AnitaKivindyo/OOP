f47da0520c5792cc9b89f27c04282b42706cdccc
anitakivindyo <anitakivindyo@gmail.com>
2022-11-01 12:53:55 +0300

    Python package - gaussian distribution

---
+++ python_package/setup.py
@@ -0,0 +1,10 @@
+import imp
+from setuptools import setup
+
+setup (
+    name='distributions',
+    version='0.1',
+    description= 'Gaussian distributions', 
+    packages= ['distributions'],
+    zip_safe = False
+ )
